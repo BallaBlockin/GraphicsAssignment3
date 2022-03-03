c08118de97b970c71b2938a826abd10e3f11dc1e
BallaBlockin <gad51261@uga.edu>
2022-03-02 22:35:39 -0500

    Added basic skeleton

---
+++ GraphicsAssignment3/src/assignment3/Main.java
@@ -0,0 +1,51 @@
+package assignment3;
+
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello");
+
+	}
+	
+	private void Inputlines (float datalines, float num) {
+	}
+	
+	private void ApplyTransformation (float[][] matrix, float datalines) {
+		
+	}
+	
+	private void Displaypixels (float datalines, float num) {
+		
+	}
+	
+	private void Outputlines (float datalines, float num) {
+		
+	}
+	
+	private void BasicTranslate (float tx, float ty) {
+		
+	}
+	
+	private void BasicScale (float sx, float sy) {
+		
+	}
+	
+	private void BasicRotate (float angle) {
+		
+	}
+	
+	private void Scale (float sx, float sy, float cx, float cy) {
+		
+	}
+	
+	private void Rotate (float angle, float cx, float cy) {
+		
+	}
+	
+	
+	
+
+}
